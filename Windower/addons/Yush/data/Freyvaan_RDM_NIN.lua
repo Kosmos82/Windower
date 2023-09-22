430aa9f70fb8b91681b1d274e66830fe02e41153
Kosmos82 <freyaru11@gmail.com>
2023-09-22 12:20:48 -0400

    initial
    initial

---
+++ Windower/addons/Yush/data/Freyvaan_RDM_NIN.lua
@@ -0,0 +1,18 @@
+Triggers = {
+    ['Alt+Ctrl+2'] = 'gs c toggle AutoShadowMode',
+    ['Alt+Ctrl+3'] = 'gs c toggle AutoFoodMode',
+    ['Alt+Ctrl+4'] = 'gs c toggle AutoBuffMode',
+    ['Alt+Ctrl+5'] = 'gs c toggle AutoWSMode',
+}
+return {
+    ['Ctrl+1'] = 'input /ma "Utsusemi: Ichi" <me>',
+    ['Ctrl+2'] = 'input /ma "Utsusemi: Ni" <me>',
+    ['Atl+1'] = 'input /ma "Dia III" <t>',
+    ['Atl+2'] = 'input /ma "Distract III" <t>',
+    ['Atl+3'] = 'input /ma "Slow II" <t>',
+    ['Atl+4'] = 'input /ma "Addle" <t>',
+    ['Atl+5'] = 'input /ws "Red Lotus Blade" <t>',
+    ['Alt+6'] = 'input /ws "Savage Blade" <t>',
+    ['Alt+7'] = 'input /ws "Sanguine Blade" <t>',
+    ['Alt+Ctrl+1'] = Triggers,
+}
